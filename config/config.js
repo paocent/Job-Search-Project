0e7355733ce2e7a43d10cf4008b38efa54ea6267
Paolo Adame <196263400+paocent@users.noreply.github.com>
2025-11-14 16:38:24 -0500

    changed removed Extra JSXs

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ env: process.env.NODE_ENV || 'development',
 port: process.env.PORT || 3000,
 jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
 mongoUri: process.env.MONGODB_URI ||
-"mongodb+srv://adamepaolo:adamepaolo1998@cluster0.xyfnver.mongodb.net/Skeleton?retryWrites=true&w=majority&appName=Cluster0" ||
+"mongodb+srv://adamepaolo:adamepaolo1998@cluster0.xyfnver.mongodb.net/JobTracker?retryWrites=true&w=majority&appName=Cluster0" ||
 process.env.MONGO_HOST ||
 'mongodb://' + (process.env.IP || 'localhost') + ':' +
 (process.env.MONGO_PORT || '27017') +
